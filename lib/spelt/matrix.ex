b1a24e38702c628afcbfb6833b921724527fb468
Jim Cain <jim@spelt.io>
2021-03-28 21:43:04 -0400

    Implemented Matrix module

---
+++ lib/spelt/matrix.ex
@@ -0,0 +1,23 @@
+defmodule Spelt.Matrix do
+  @moduledoc """
+  Implements utility functions related to Matrix
+  """
+
+  @fq_pattern ~r{^(@?)([-a-z0-9._=/]+)(:([-a-zA-Z0-9.]+))?$}
+
+  def user_to_fq_user_id(%{host: hostname}, username) do
+    case split_user_id(username) do
+      [user, nil] -> "@#{user}:#{hostname}"
+      [_user, _host] -> username
+      nil -> nil
+    end
+  end
+
+  def split_user_id(username) do
+    case Regex.run(@fq_pattern, username) do
+      [_, _, user, _, host] -> [user, host]
+      [_, _, user] -> [user, nil]
+      _ -> nil
+    end
+  end
+end
---
+++ test/spelt/matrix_test.exs
@@ -0,0 +1,30 @@
+defmodule Spelt.MatrixTest do
+  use ExUnit.Case
+
+  alias Spelt.Matrix
+
+  describe "Matrix.split_user_id/1" do
+    test "with invalid user name, returns nil" do
+      assert Matrix.split_user_id("!") == nil
+      assert Matrix.split_user_id("@") == nil
+      assert Matrix.split_user_id(":") == nil
+    end
+
+    test "with local user name part, returns the local user name part" do
+      assert ["phred", nil] = Matrix.split_user_id("phred")
+      assert ["phred", nil] = Matrix.split_user_id("@phred")
+    end
+
+    test "with fully qualified user name, returns the user and hostname parts" do
+      assert ["phred.smerd", "example.net"] = Matrix.split_user_id("@phred.smerd:example.net")
+    end
+  end
+
+  describe "Matrix.user_to_fq_user_id/2" do
+    test "returns the fully qualified user ID" do
+      conn = %{host: "foobar.net"}
+      assert Matrix.user_to_fq_user_id(conn, "phred.smerd") == "@phred.smerd:foobar.net"
+      assert Matrix.user_to_fq_user_id(conn, "@phred.smerd:example.cc") == "@phred.smerd:example.cc"
+    end
+  end
+end
